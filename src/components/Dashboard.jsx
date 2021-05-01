157cb57e3ee8ae1929693ffee5da2570ac7ea315
sandrossano <32746792+sandrossano@users.noreply.github.com>
2021-05-01 17:13:58 +0200

    Add files via upload

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -10,7 +10,8 @@ class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     if (window.sessionStorage.getItem("logged") !== "X") {
-      window.open("/login", "_self");
+      window.location.href = "/login";
+      //window.open("/login", "_self");
     }
     window.sessionStorage.getItem("user");
     this.state = {};
